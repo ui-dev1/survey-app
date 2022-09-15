f21c5dc4c36d2a66d0ed14ab6183e42236138bd4
Shipra Vishnoi <shipra.vishnoi@whitehatjr.com>
2022-09-15 17:56:28 +0530

    query check added

--- src/components/Layout/Discussion/Discussion.jsx
+++ src/components/Layout/Discussion/Discussion.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import {
     Card,
     CardContent,
@@ -18,10 +18,12 @@ import SearchIcon from '@mui/icons-material/Search';
 import {
     uniq
 } from "lodash";
+import PrescribingBehaviour from './PrescribingBehaviour/PrescribingBehaviour';
 
 const Discussion = () => {
     const dispatch = useDispatch();
     const history = useNavigate();
+    const [searchParams, setSearchParams] = useSearchParams();
     useEffect(() => {
         dispatch(guideActions.getDiscussionGuideData());
 
@@ -69,65 +71,72 @@ const Discussion = () => {
             >
                 Create a Discussion Guide
             </Typography>
-            <Box className='box__container'>
-                <Paper
-                    component="form"
-                    className="search__container"
-                >
-                    <IconButton type="button" aria-label="search" className='icon__button'>
-                        <SearchIcon />
-                    </IconButton>
-                    <InputBase
-                        sx={{ ml: 1, flex: 1 }}
-                        placeholder="Search Business Objectives"
-                        inputProps={{ 'aria-label': 'search' }}
-                        value={searchValue}
-                        onChange={handleSearch}
-                        className="search"
-                    />
-                </Paper>
-            </Box>
-            <Grid container spacing={1}>
-                {
-                    discussionData.map((data) => {
-                        return (
-                            <Grid item xs={3}>
-                                <Card sx={{ borderRadius: '16px' }} variant='outlined' className='card__container'>
-                                    <CardContent>
-                                        <Typography
-                                            variant="caption"
-                                            gutterBottom
-                                            className='content__pos period__color'
-                                        >
-                                            {data.modifiedOn} ago
-                                        </Typography>
-                                        <Typography
-                                            variant="h6"
-                                            gutterBottom
-                                        >
-                                            {data.name}
-                                        </Typography>
-                                        <Typography
-                                            variant="body2"
-                                            gutterBottom
-                                            className='desc__color'
-                                        >
-                                            {data.description}
-                                        </Typography>
-                                        <Link
-                                            underline="none"
-                                            className='content__pos txt__link'
-                                            onClick={() => handleLink(data.id)}
-                                        >
-                                            Select
-                                        </Link>
-                                    </CardContent>
-                                </Card>
-                            </Grid>
-                        )
-                    })
-                }
-            </Grid>
+            {
+                searchParams.get("businessId") ?
+                    <PrescribingBehaviour />
+                :
+                <>
+                    <Box className='box__container'>
+                        <Paper
+                            component="form"
+                            className="search__container"
+                        >
+                            <IconButton type="button" aria-label="search" className='icon__button'>
+                                <SearchIcon />
+                            </IconButton>
+                            <InputBase
+                                sx={{ ml: 1, flex: 1 }}
+                                placeholder="Search Business Objectives"
+                                inputProps={{ 'aria-label': 'search' }}
+                                value={searchValue}
+                                onChange={handleSearch}
+                                className="search"
+                            />
+                        </Paper>
+                    </Box>
+                    <Grid container spacing={1}>
+                        {
+                            discussionData.map((data) => {
+                                return (
+                                    <Grid item xs={3}>
+                                        <Card sx={{ borderRadius: '16px' }} variant='outlined' className='card__container'>
+                                            <CardContent>
+                                                <Typography
+                                                    variant="caption"
+                                                    gutterBottom
+                                                    className='content__pos period__color'
+                                                >
+                                                    {data.modifiedOn} ago
+                                                </Typography>
+                                                <Typography
+                                                    variant="h6"
+                                                    gutterBottom
+                                                >
+                                                    {data.name}
+                                                </Typography>
+                                                <Typography
+                                                    variant="body2"
+                                                    gutterBottom
+                                                    className='desc__color'
+                                                >
+                                                    {data.description}
+                                                </Typography>
+                                                <Link
+                                                    underline="none"
+                                                    className='content__pos txt__link'
+                                                    onClick={() => handleLink(data.id)}
+                                                >
+                                                    Select
+                                                </Link>
+                                            </CardContent>
+                                        </Card>
+                                    </Grid>
+                                )
+                            })
+                        }
+                    </Grid>
+                </>
+            }
         </div>
     )
 }
---
+++ src/components/Layout/Discussion/PrescribingBehaviour/PrescribingBehaviour.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PrescribingBehaviour = () => {
+    return (
+        <div>Prescribing Behaviour</div>
+    )
+}
+
+export default PrescribingBehaviour
